f849592b10fc1518dbd9ac4307becb941e4e01e2
olayinka06-dev <olayinkaconsult06@gmail.com>
2023-05-15 09:18:57 -0700

    making eandom adjustment

--- src/Component/Footer.jsx
+++ src/Component/Footer.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import styled from 'styled-components';
-import {FaFacebookSquare, FaInstagram, FaPinterest, FaTwitter, Fainfocircle} from 'react-icons/fa'
+import {FaFacebookSquare, FaInstagram, FaPinterest, FaTwitter } from 'react-icons/fa'
 
 const Footer = () => {
   return (
