deb2c890c74e7efe3202aef21f2c2ee5cb912296
olayinka06-dev <olayinkaconsult06@gmail.com>
2023-05-20 06:38:33 -0700

    making some random adjustment

--- src/Component/Boost page/Boost.jsx
+++ src/Component/Boost page/Boost.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import styled from 'styled-components';
-import boost from './images/bg-boost-desktop.svg'
+import boost from '../images/bg-boost-desktop.svg'
 
 const Boost = () => {
   return (
